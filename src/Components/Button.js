a8fcfb2836574f36ac8345529aa7f6283c29d5ad
sonal <sonal.cmendis@gmail.com>
2023-07-02 21:27:57 +0530

    small update

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -1,31 +1,44 @@
 import React from "react";
 import styled from "styled-components/macro";
-import { Link } from "react-router-dom";
 import * as GlobalVariables from "../Styles/GlobalVariables";
-
 const ButtonStyled = styled.div`
+  display: block;
+  @media ${GlobalVariables.device.laptop} {
+    display: inline-block;
+  }
+  margin-top: 1rem;
+
+  display: inline-block;
   // styles for a basic minimal rounded corner button
   background: transparent;
-  border-radius: 0.25em;
-  padding: 1em 2em;
-  font-size: 1em;
+
+  padding: 0.9rem 2rem;
+  font-size: 0.8rem;
   font-family: ${GlobalVariables.fonts.primary};
+
   outline: none;
-  border: 1px solid white;
-  display: inline-block;
-  margin-top: 1rem;
+  border: 1px solid black;
   cursor: pointer;
+  text-transform: uppercase;
+  letter-spacing: 2px;
+  border-radius: 100vw;
+  transition: all 0.15s;
+  color: black;
+  &:hover {
+  }
+
+  cursor: pointer;
+  a {
+  }
 `;
 
 const Button = (props) => {
   return (
-    <Link to={props.href}>
-      <ButtonStyled type={props.type}>
-        {
-          props.children /* this injects the content from wherever its used as a HOC, that way you can call the button component and put your own text */
-        }
-      </ButtonStyled>
-    </Link>
+    <ButtonStyled className="button" type={props.type} onClick={props.onClick}>
+      {
+        props.children /* this injects the content from wherever its used as a HOC, that way you can call the button component and put your own text */
+      }
+    </ButtonStyled>
   );
 };
 
